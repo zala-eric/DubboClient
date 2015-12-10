3633a19b599b82bfd686201abd37db6f683a980e
zala-eric <zala.eric@gmail.com>
2015-12-10 11:24:20 +0800

    首次提交代码

---
+++ src/main/java/com/tydic/client/dubbo/action/ChatAction.java
@@ -0,0 +1,21 @@
+package com.tydic.client.dubbo.action;
+
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.tydic.server.dubbo.service.DemoServer;
+
+public class ChatAction {
+	private Logger log = LoggerFactory.getLogger(this.getClass());
+	@SuppressWarnings("resource")
+	public void SayHello(){   
+	    ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[] { "dubboClient.test.xml" });  
+	    context.start();  
+	    DemoServer demoServer = (DemoServer) context.getBean("demoService");  
+	    
+	    log.info("client:"+demoServer.sayHello("Morning"+"1:"+new Date())+"--3:"+new Date());
+	    }  
+}
---
+++ src/main/java/com/tydic/client/dubbo/main/ClientStart.java
@@ -0,0 +1,12 @@
+package com.tydic.client.dubbo.main;
+
+import com.tydic.client.dubbo.action.ChatAction;
+
+public class ClientStart {
+
+	public static void main(String[] args) {
+		ChatAction act = new ChatAction();  
+        act.SayHello(); 
+	}
+
+}
